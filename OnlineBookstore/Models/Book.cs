534daa4d44cc40181bebf11cd27bbb8b571aec45
parkerlg <69094049+parkerlg@users.noreply.github.com>
2021-02-17 11:06:28 -0700

    Initial commit

---
+++ OnlineBookstore/Models/Book.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace OnlineBookstore.Models
+{
+    public class Book
+    {
+        [Key]
+        [Required]
+        public int BookId { get; set; }
+        [Required]
+        public string Title { get; set; }
+        [Required]
+        public string Author { get; set; }
+        [Required]
+        public string Publisher { get; set; }
+        [Required]
+        public string ISBN { get; set; }
+        [Required]
+        public string Category { get; set; }
+        [Required]
+        public double Price { get; set; }
+
+    }
+}
---
+++ OnlineBookstore/Models/SeedData.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+using System.Linq;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
+namespace OnlineBookstore.Models
+{
+    public class SeedData
+    {
+        public static void EnsurePopulated(IApplicationBuilder application)
+        {
+            BookstoreDBContext context = application.ApplicationServices.CreateScope().ServiceProvider.GetRequiredService<BookstoreDBContext>();
+            if (context.Database.GetPendingMigrations().Any())
+            {
+                context.Database.Migrate();
+            }
+            if (!context.Books.Any())
+            {
+                context.Books.AddRange(
+
+                    new Book
+                    {
+                        Title = "Les Miserables",
+                        Author = "Victor Hugo",
+                        Publisher = "Signet",
+                        ISBN = "978-0451419439",
+                        Category = "Fiction",
+                        Price = 9.95
+                    },
+
+                    new Book
+                    {
+                        Title = "Les Miserables",
+                        Author = "Victor Hugo",
+                        Publisher = "Signet",
+                        ISBN = "978-0451419439",
+                        Category = "Classic",
+                        Price = 9.95
+                    }
+                    );
+                context.SaveChanges();
+
+            }
+        }
+    }
+}
